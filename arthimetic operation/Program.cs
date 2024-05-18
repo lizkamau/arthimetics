0584f1ac47dc28ebf15c5681742339d51cd05208
lizkamau <nyaruakamau@gmail.com>
2024-05-18 22:37:05 +0300

    first commit

---
+++ arthimetic operation/Program.cs
@@ -0,0 +1,62 @@
+﻿
+//addition
+
+//using System.Diagnostics.CodeAnalysis;
+
+int student = 15;
+//studen=student +5;
+//student +=25;
+// student++;
+//student = student + 1; this is the standard form
+//student += 10; addition shortcut
+//student++; used when using loops
+Console.WriteLine(student);
+
+Console.WriteLine("hey this is addition");
+//Console.WriteLine("please enter a number");
+int x=20, y=30; 
+int z=x+y;
+Console.WriteLine(z);
+
+
+
+//subtraction
+Console.WriteLine("this is subtraction");
+//student = student - 10;
+//student--; //deletes one num at a time
+//Console.WriteLine(student);
+
+int a = 20, b= 30;
+int c= a-b; 
+Console.WriteLine(c);   
+
+Console.WriteLine("this is multiplaction");
+//multiplaction
+
+//student = student * 3;
+//student =2 *2;
+//Console.WriteLine(student);
+int w= 20, h= 30;   
+int i= w*h;
+Console.WriteLine(i);   
+
+
+
+Console.WriteLine("this is division");
+//division
+//student = 30 / 10;
+//student = student / 5;
+//student /= 5;
+//student /= 7; saves it as integer as a whole num
+//Console.Write(student);
+int k=200, l= 30;    
+int m= k/l;
+Console.WriteLine(m);   
+
+
+
+
+
+
+
+
